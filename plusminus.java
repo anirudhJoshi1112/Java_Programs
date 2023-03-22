6f2a291338a0204b0bc59a7cf405fb74caf68e57
anirudhJoshi1112 <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-03-22 17:03:39 +0000

    basic1

---
+++ plusminus.java
@@ -0,0 +1,9 @@
+class plusminus{
+    public static void main(String[] args){
+        int a=10,b=20;
+        int sum=a+b;
+        int ans=a+b/a-b;
+        System.out.println(sum);
+        System.out.println(ans);
+    }
+}
