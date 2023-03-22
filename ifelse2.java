3661dc5f9571522d06e791da67c36872f05a42e7
anirudhJoshi1112 <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-03-22 23:13:17 +0530

    even odd

---
+++ ifelse2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x%2==0){
+            System.out.println("no.is even");
+        }
+        else{
+            System.out.println("no. is odd");
+        }
+    }
+}
