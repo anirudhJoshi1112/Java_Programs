0f42de5b82f793eeee39df43889aaae06df0872e
anirudhJoshi1112 <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-03-22 17:15:00 +0000

    scanner ka use1

---
+++ util.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //String name = sc.next(); it gives only a single token
+        String name = sc.nextLine(); 
+        System.out.println(name);
+    }
+}
