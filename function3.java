3d3d409d37010b4c81339b10fec710acb102c76b
anirudhJoshi1112 <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-03-23 18:39:27 +0000

    finding factorial using function

---
+++ function3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class main{
+    public static void printfactorial(int n){
+        if(n<0){
+            System.out.println("invalid case");
+            return;
+        }
+        int factorial=1;
+        for(int i=n;i>=1;i--){
+            factorial=factorial*i;
+        }
+        System.out.println(factorial);
+        return;
+    }
+    public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            printfactorial(n); //function calling
+        }
+}
