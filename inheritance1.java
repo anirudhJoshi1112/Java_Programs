3f3583b9d416e5c8644c75b59243bfbd43f66b41
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-04-03 10:21:45 +0000

    types of inheritance

---
+++ inheritance1.java
@@ -0,0 +1,26 @@
+//single level inheritance
+class shape{
+    public void area(){
+        System.out.println("display area");
+    }
+}
+class singlelevel extends shape{
+    public void area(int l,int h){
+        system.out.println(1/2*l*h);
+    }
+}
+class multilevel extends shape{
+    public void area(int l,inth){
+        system.out.println(1/2*l*h);
+    }
+}
+class hierarchial extends shape{
+    public void area(int r){
+        system.out.println(3.14*r*r);
+    }
+}
+public class oops{
+    public static void main(String args[]){
+
+    }
+}
